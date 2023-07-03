76fc2ed2e1fd3ba56b3d2bb74f0d8ae5599351b2
iamalexkorotin <iamalexkorotin@gmail.com>
2023-07-03 14:20:58 +0000

    Added missing 'DatasetSampler'

--- src/distributions.py
+++ src/distributions.py
@@ -16,6 +16,24 @@ class Sampler:
     def sample(self, size=5):
         pass
     
+class DatasetSampler(Sampler):
+    def __init__(self, dataset, num_workers=40, device='cuda'):
+        super(DatasetSampler, self).__init__(device=device)
+        
+        loader = torch.utils.data.DataLoader(dataset, batch_size=num_workers, num_workers=num_workers)
+        
+        with torch.no_grad():
+            self.dataset = torch.cat(
+                [X for (X, y) in loader]
+            )
+        
+    def sample(self, batch_size=16):
+        ind = random.choices(range(len(self.dataset)), k=batch_size)
+        with torch.no_grad():
+            batch = self.dataset[ind].clone().to(self.device).float()
+        return batch
+    
+    
 class StandardNormalSampler(Sampler):
     def __init__(
         self, dim=2, device='cuda'
